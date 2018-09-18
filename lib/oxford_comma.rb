78cba49936d921547d1830721bc81ef10f6d07ab
Sean Martin <sean.martin@gmail.com>
2018-09-18 21:21:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,10 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
+  elsif
+    message = array.slice(0..-2).join(", ")
+    message += " and "
+    message += array.slice(-1)
+    message
   end
 end
