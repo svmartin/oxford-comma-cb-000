a11c994fc71a46e02ed435d734acc70705d950e1
Sean Martin <sean.martin@gmail.com>
2018-09-18 21:23:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size <= 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif
+    message = array.slice(0..-2).join(", ")
+    message += ", and "
+    message += array.slice(-1)
+    message
+  end
 end
